2a8c135df3c28c18ec1e401d44315e559144ba18
chenMM <1197729488@qq.com>
2020-06-26 09:14:06 +0800

    update

--- 02_Blob&ArrayBuffer&FileReader&File&URL&WebWorker/ArrayBuffer/index.ts
+++ 02_Blob&ArrayBuffer&FileReader&File&URL&WebWorker/ArrayBuffer/index.ts
@@ -1,28 +1,25 @@
 /**
- * Convert ArrayBuffer/TypedArray to String via TextDecoder
- *
- * @see https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder
+ * 将ArrayBuffer对象、TypedArray对象转换为字符串
+ * @param input
+ * @param outputEncoding 编码方式
+ * @return {String}
  */
 function ab2str(
     input: ArrayBuffer | Uint8Array | Int8Array | Uint16Array | Int16Array | Uint32Array | Int32Array,
     outputEncoding: string = 'utf8',
 ): string {
-    const decoder = new TextDecoder(outputEncoding)
-    return decoder.decode(input)
+  const decoder = new TextDecoder(outputEncoding)
+  return decoder.decode(input)
 }
 
 /**
- * Convert String to ArrayBuffer via TextEncoder
- *
- * @see https://developer.mozilla.org/zh-CN/docs/Web/API/TextEncoder
+ * 将字符串转换为ArrayBuffer对象
+ * @param input
+ * @return {ArrayBuffer}
  */
 function str2ab(input: string): ArrayBuffer {
-    const view = str2Uint8Array(input)
-    return view.buffer
-}
-
-/** Convert String to Uint8Array */
-function str2Uint8Array(input: string): Uint8Array {
-    const encoder = new TextEncoder()
-    return encoder.encode(input)
+  //将字符串转换为Uint8Array
+  const uInt8Array = new TextEncoder().encode(input)
+  //从Uint8Array对象得到ArrayBuffer实例
+  return uInt8Array.buffer
 }
